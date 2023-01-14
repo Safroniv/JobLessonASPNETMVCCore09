bb28c0d35d714c8bf144228c8b1f9dcaa1ececb7
Иван Сафронов <safroniv@mail.ru>
2023-01-14 22:21:25 +0300

    Create empty ASP NET

--- JobLessonASPNETMVCCore09v01/Program.cs
+++ JobLessonASPNETMVCCore09v01/Program.cs
@@ -5,9 +5,23 @@ namespace JobLessonASPNETMVCCore09v01
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            //app.MapGet("/", () => "Hello World!");
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapDefaultControllerRoute();
 
             app.Run();
         }
